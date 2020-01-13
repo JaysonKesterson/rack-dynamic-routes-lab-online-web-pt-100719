30fce91ac50da7e70a23ef95af9910744f2ee8e8
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-13 22:06:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,10 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
+         resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 404
     else
       resp.write "Route not found"
       resp.status = 404
